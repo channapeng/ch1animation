19ac7b10cf8bba0dcfa826828c7c0142b0157302
channapeng <nikornchannapeng>
2022-04-27 13:30:10 +0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  semi: [2, 'never'],
+};
+
+
+// module.exports = {
+//   root: true,
+//   extends: '@react-native-community',
+// };
---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  semi: false,
+};
+
+
+// module.exports = {
+//   bracketSpacing: false,
+//   jsxBracketSameLine: true,
+//   singleQuote: true,
+//   trailingComma: 'all',
+//   arrowParens: 'avoid',
+// };
